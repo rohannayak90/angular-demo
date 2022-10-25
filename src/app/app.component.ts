d8304a7ee238ddeaefb7793ecc6a903f6051b5e3
rohannayak90 <rohannayak90@gmail.com>
2022-10-25 11:48:56 +0530

    Popup using Angular Material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+
+import { ConfirmationModalComponent } from './components/confirmation-modal/confirmation-modal.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-demo';
+  dialogRef: MatDialogRef<any, any> | undefined;
+
+  constructor(private dialog: MatDialog) {}
+
+  openModal() {
+    this.dialogRef = this.dialog.open(ConfirmationModalComponent, {
+      // height: '400px', width: '600px',
+      data: 'Sam'
+    });
+    this.dialogRef.afterClosed().subscribe((response: string) => {
+      console.log('closed', response);
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ConfirmationModalComponent } from './components/confirmation-modal/confirmation-modal.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConfirmationModalComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatDialogModule,
+    AppRoutingModule,
   ],
   providers: [],
+  entryComponents: [
+    ConfirmationModalComponent,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
