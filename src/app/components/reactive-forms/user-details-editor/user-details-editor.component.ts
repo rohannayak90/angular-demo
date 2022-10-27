05267bf371b05fcfddf7cc67f6973e47b27ae472
Murari Nayak <rohannayak90@gmail.com>
2022-10-27 19:51:19 +0530

    Reactive form changes part 2

--- src/app/components/reactive-forms/user-details-editor/user-details-editor.component.ts
+++ src/app/components/reactive-forms/user-details-editor/user-details-editor.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-user-details-editor',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserDetailsEditorComponent implements OnInit {
 
+  userDetailsForm = new FormGroup({
+    name: new FormControl('Sam'),
+    age: new FormControl(10)
+  });
+  
   constructor() { }
 
   ngOnInit(): void {
+    this.userDetailsForm.valueChanges.subscribe(changedValues => {
+      console.log('changed values', changedValues);
+    });
+    // (this.userDetailsForm.get('name') as FormControl).valueChanges.subscribe(changedValues => {
+    //   console.log('changing name', changedValues);
+    // });
+  }
+
+  onSubmitClick() {
+    console.log('name', this.userDetailsForm.value.name)
+    console.log('age', this.userDetailsForm.value.age)
   }
 
 }
